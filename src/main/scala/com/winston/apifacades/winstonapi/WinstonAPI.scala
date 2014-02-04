9ab0a125291d17748f54ccd6b8e0fa776bc01a6f
keco1249 <kcolin1@gmail.com>
2014-02-04 13:58:15 -0700

    briefing format and name scrubbing

--- src/main/scala/com/winston/apifacades/winstonapi/WinstonAPI.scala
+++ src/main/scala/com/winston/apifacades/winstonapi/WinstonAPI.scala
@@ -22,7 +22,7 @@ class WinstonAPI {
 	}
 	
 //	def briefingCall(loc:String, timezone:Int, fbToken:String, twitterToken:String, twitterSecret:String):ArrayList[Object] = {
-	def briefingCall(creds:UserCredentials):ArrayList[Object] = {
+	def briefingCall2(creds:UserCredentials):ArrayList[Object] = {
 	  var url = baseUrl + "/brief?"
 	  if(creds.udid != null)
 	    url += ("UDID="+ creds.udid + "&")
@@ -45,5 +45,54 @@ class WinstonAPI {
 		  briefSet.add(data)
 	  
 	  return briefSet
+	}
+	
+	def briefingCall(creds:UserCredentials):ArrayList[ArrayList[Object]] = {
+	  var url = baseUrl + "/brief?"
+	  if(creds.udid != null)
+	    url += ("UDID="+ creds.udid + "&")
+	  if(creds.loc != null)
+	    url += ("loc="+ creds.loc + "&")
+	  if(creds.facebook_token != null)
+	    url += ("facebook_token="+ creds.facebook_token + "&")
+	  if(creds.twitter_token != null)
+	    url += ("twitter_token="+ creds.twitter_token + "&")
+	  if(creds.twitter_secret != null)
+	    url += ("twitter_token_secret="+ creds.twitter_secret)	
+	  url += ("timezone_offset="+ creds.timezone_offset + "&")
+	  
+	  var response = Tools.fetchURL(url)
+	  if(!response.has("data"))
+	    return null
+	  
+	  var briefSet = new ArrayList[ArrayList[Object]]
+	  var weatherSet = new ArrayList[Object]
+	  var newsSet = new ArrayList[Object]
+	  var twitterSet = new ArrayList[Object]
+	  var fbSet = new ArrayList[Object]
+	  
+	  weatherSet = weatherCall(creds.loc, creds.timezone_offset)
+	  
+	  for(data <- response.get("data")){
+	    if(data.has("type")){
+	      data.get("type").asText() match{
+	        case "News" => newsSet.add(data)
+	        case "Facebook" => fbSet.add(data)
+	        case "Twitter" => twitterSet.add(data)
+	        case a:Any => println("Not added - " + a)
+	      }
+	    }
+	  }
+	  
+	  if(weatherSet != null && !weatherSet.isEmpty())
+	    briefSet.add(weatherSet)
+	  if(!newsSet.isEmpty())
+	    briefSet.add(newsSet)
+	  if(!twitterSet.isEmpty())
+	    briefSet.add(twitterSet)
+	  if(!fbSet.isEmpty())
+	    briefSet.add(fbSet)
+
+	  briefSet
 	}
 }
--- src/main/scala/com/winston/user/Name.scala
+++ src/main/scala/com/winston/user/Name.scala
@@ -1,8 +1,11 @@
 package com.winston.user
 
 import com.winston.store.MongoStore
+import org.codehaus.jackson.map.ObjectMapper
 
 class Name {
+  @transient
+  val mapper = new ObjectMapper()
   var string:String = null
   var scrubbed:String = null
   var mongo = new MongoStore("names")
@@ -11,7 +14,22 @@ class Name {
     this()
     mongo = new MongoStore("names")
     this.string = string
+    this.scrubbed = scrubName(string)
+  }
+  
+  def this(string:String, scrubbed:String){
+    this()
+    this.string = string
+    this.scrubbed = scrubbed
+  }
+  
+  def scrubName(name:String):String = {
+    var obj = mongo.findOneSimple("name", name)
+    if(obj == null)
+      return name
+    var cleanJson = obj.toString().replaceAll("\\r", " ").replaceAll("\\n", " ").trim
+    val nameJson = mapper.readTree(cleanJson);	
     
-    
+    return nameJson.get("pronounce").asText()
   }
 }
